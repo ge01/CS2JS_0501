9f944593db0dc0bab55be73bf53305836554c148
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-21 14:20:31 -0500

    working app

---
+++ app.js
@@ -0,0 +1,43 @@
+// Event listener that fires when a user clicks a button.
+document.getElementById("myBtn").addEventListener("click", function(){
+  computeBalance();
+});
+
+// Constant for the monthly interest rate
+var INTEREST_RATE = 0.005;
+
+// Global variables
+var balance;      // The account balance
+var months;       // The number of months
+var count = 1;    // Loop counter, initialized with 1
+
+function computeBalance(){
+  // Get the starting balance
+  balance = parseFloat(document.getElementById('balance').value);
+
+  // Get the number of months
+  months =  parseFloat(document.getElementById('months').value);
+
+  // Validate data entered
+  if (balance === null || balance === "" || isNaN(balance)) {
+    // Display an error message for miles
+    document.getElementById('outBalance').innerHTML = "Balance must be filled out!";
+  }
+  else if (months === null || months === "" || isNaN(months)) {
+    // Display an error message for gallons
+    document.getElementById('outBalance').innerHTML = "Months must be filled out!";
+  }
+  else {
+    // The following loop calculates the ending balance
+    while (count <= months) {
+      // Add this month's interest to the balance
+      balance = balance + (INTEREST_RATE * balance);
+
+      // Add one to the loop counter.
+      count = count + 1;
+    }
+
+    // Display the ending balance
+    document.getElementById('outBalance').innerHTML = "$" +  balance.toFixed(2);
+  }
+}
